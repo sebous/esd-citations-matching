87a8c15b111eed359150bd7927c384aa33cbf740
sebous <sebous@gmail.com>
2022-01-19 01:58:36 +0100

    benchmarks

--- cases-worker/src/lib/benchmark.rs
+++ cases-worker/src/lib/benchmark.rs
@@ -7,6 +7,8 @@ extern crate test;
 lazy_static! {
     pub static ref INPUT: String = read_to_string("test_data.txt").unwrap();
     pub static ref REG: Regex = Regex::new(r"Wightman a další").unwrap();
+    pub static ref REG_LOWER: Regex =
+        Regex::new(format!(r"Wightman a další").to_lowercase().as_str()).unwrap();
 }
 
 #[cfg(test)]
@@ -20,6 +22,24 @@ mod tests {
     }
     #[bench]
     fn regex(b: &mut Bencher) {
-        b.iter(|| Regex::new(r"Wightman a další").unwrap().is_match(&INPUT))
+        b.iter(|| REG.is_match(&INPUT))
+    }
+
+    #[bench]
+    fn regex_lower(b: &mut Bencher) {
+        let input = INPUT.to_lowercase();
+        b.iter(|| REG_LOWER.is_match(input.as_str()))
+    }
+
+    #[bench]
+    fn contains_bytes(b: &mut Bencher) {
+        let input = INPUT.to_lowercase();
+        let search = "Wightman a další".to_lowercase();
+        b.iter(|| {
+            input
+                .as_bytes()
+                .windows(search.len())
+                .position(|window| window == search.as_bytes())
+        })
     }
 }
