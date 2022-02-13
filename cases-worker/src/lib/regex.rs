8a760e086cf5b0eeda8e9f84f48eb7e99b16d69e
sebous <sebous@gmail.com>
2022-02-13 18:51:18 +0100

    added comment to regex

--- cases-worker/src/lib/regex.rs
+++ cases-worker/src/lib/regex.rs
@@ -4,17 +4,17 @@ use regex::Regex;
 use super::db;
 
 lazy_static! {
+    // u2012 -> u002D - various "dash" characters
+
     /// C-XXX/XX
     pub static ref C_CODE: Regex =
-        Regex::new(r"(C\s*[--]\s*\d{1,4}[/\--]\d{1,2})([ \u202F\u00A0,.)]|$)").unwrap();
+        Regex::new(r"(C\s*[\u2010\u2011\u2012\u2013\u2014\u2212\u002D]\s*\d{1,4}[/\u2010\u2011\u2012\u2013\u2014\u2212\u002D]\d{1,2})([ \u202F\u00A0,.)]|$)").unwrap();
     /// T-XXX/XX
     pub static ref T_CODE: Regex =
-        Regex::new(r"(T\s*[--]\s*\d{1,4}[/\--]\d{1,2})([ \u202F\u00A0,.)]|$)").unwrap();
+        Regex::new(r"(T\s*[\u2010\u2011\u2012\u2013\u2014\u2212\u002D]\s*\d{1,4}[/\u2010\u2011\u2012\u2013\u2014\u2212\u002D]\d{1,2})([ \u202F\u00A0,.)]|$)").unwrap();
     /// num only XXX/XX
     pub static ref CODE: Regex =
-        Regex::new(r"[\s\u202F\u00A0](\d{1,4}[/\--]\d{2})([\s\u202F\u00A0,.)]|$)").unwrap();
-
-    pub static ref VS: Regex = Regex::new(r"\s(v.)\s").unwrap();
+        Regex::new(r"[\s\u202F\u00A0](\d{1,4}[/\u2010\u2011\u2012\u2013\u2014\u2212\u002D]\d{2})([\s\u202F\u00A0,.)]|$)").unwrap();
 }
 
 pub fn gen_shname_regx(data: &Vec<db::EsdCase>) -> Vec<(usize, Regex)> {
